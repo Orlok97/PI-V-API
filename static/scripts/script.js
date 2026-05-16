633ba40a68bfad19c8756795c6c7cc66b7a63a42
Orlok <cristian.braz97@gmail.com>
2026-05-16 19:13:21 -0300

    feat: create dashboard, add request scripts

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,1 +1,25 @@
-console.log('hello world!');
+const temperatura=document.querySelector('#temperatura');
+const humidade=document.querySelector('#humidade');
+const qualidade_ar=document.querySelector('#qualidade_ar');
+
+const getCurrentTemp=async ()=>{
+    try{
+        const response=await fetch('/api/v1/sensor/current-temp')
+        if(!response.ok){
+            throw new Error('erro ao buscar os dados!')
+        }
+        const data=await response.json()
+        if(data){
+            temperatura.innerHTML=`${data.temperature}°C`;
+            humidade.innerHTML=`${data.humidity}%`;
+        }
+
+    }catch(err){
+        console.error('Erro na requisição '+err)
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  getCurrentTemp();
+  setInterval(getCurrentTemp, 5000);
+});
